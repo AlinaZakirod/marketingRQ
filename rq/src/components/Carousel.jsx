c3a0b001ee328f3f38185e640d3bfe078076557a
AlinaZakirod <gabie1989@gmail.com>
2020-01-17 16:48:07 -0500

    structure for home page

---
+++ rq/src/components/Carousel.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Carousel extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p>Carousel</p>
+        <p>Features properties</p>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
---
+++ rq/src/components/Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const info = () => {
+  return (
+    <div>
+      <h1 className="App-title">About ReloQuest</h1>
+      <h3>Section fullscreen.Info </h3>
+    </div>
+  );
+};
+
+export default info;
