3d6738d1c3378750e0f9f4bdeaee5791402e4879
AlinaZakirod <gabie1989@gmail.com>
2020-02-18 17:10:34 -0500

    home page content

---
+++ rq/src/components/Features.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <section>
+      <img />
+      <h3>B2E features</h3>
+      <p>
+        ReloQuest is elevating Client Verified Suppliers by implementing the
+        eloquent Verification. This will enable RQ Clients to search with
+        confidence and view those Suppliers that have taken an extra step
+      </p>
+    </section>
+  );
+};
+
+export default Features;
--- rq/src/components/Info.jsx
+++ rq/src/components/Info.jsx
@@ -3,8 +3,20 @@ import React from "react";
 const info = () => {
   return (
     <div>
-      <h1 className="App-title">About ReloQuest</h1>
-      <h3>Section fullscreen.Info </h3>
+      <h1 className="App-title">How it Works?</h1>
+      <h3>Scroll horizontal</h3>
+      <p>
+        ReloQuest® services provide an independent Cloud-based sourcing platform
+        for the Temporary Housing and Serviced Apartment Industry that offers
+        real-time, unbiased options. ReloQuest offers suppliers the ability to
+        become educated and more competitive in the marketplace while at the
+        same time ReloQuest significantly reduces the cost to the client.The
+        universal ReloQuest platform integrates Clients, Suppliers, and Guests
+        throughout the process of their accommodation.
+        <section>Search</section>
+        <section>Compare</section>
+        <section>Book</section>
+      </p>
     </div>
   );
 };
