7cbfaffb036fdb24da533a7b08a45b162c5405b8
AlinaZakirod <gabie1989@gmail.com>
2020-02-25 16:30:46 -0500

    featured images added pictures

--- rq/src/components/Carousel.jsx
+++ rq/src/components/Carousel.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 
 class Carousel extends Component {
-  state = {};
+  playCarousel() {
+    let x = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+  }
   render() {
     return (
       <div>
         <p>Carousel</p>
         <p>Features properties</p>
+        <img
+          className="mySlides"
+          src="../../images/amoon-ra-laY6t1sagU8-unsplash.jpg"
+        />
+        <img
+          className="mySlides"
+          src="../../images/anmol-seth-hDbCjHNdF48-unsplash.jpg"
+        />
+        <img
+          className="mySlides"
+          src="../../images/apartment-architecture-cabinets-chairs-533157.jpg"
+        />
       </div>
     );
   }
